54eb8f5c15b1ca3557a651303c6f6b02d6c1ff37
Gabriel L Calsada <gabriellcalsada@gmail.com>
2020-11-14 12:22:55 -0300

    Primeiro endpoint funcionando /casais/churn

--- src/controllers/casais/casais.controller.js
+++ src/controllers/casais/casais.controller.js
@@ -1,8 +1,53 @@
-const lejour = require('../../../services/api/lejour');
+const lejour = require('../../services/api/lejour');
+const moment = require('moment');
+
+function obtemCasamentosComData(weddings) {
+    return weddings.filter((wedding) => {
+        return wedding.WEDDING_DATE !== 'NULL';
+    });
+}
+
+function obtemCasamentosSemInvoice(weddings, invoices) {
+    return weddings.filter(wedding => {
+        return !invoices.find(invoice => invoice.WEDDING_ID == wedding.ID);
+    });
+}
+
+function obtemCasamentosComInvoicesRecusadas(weddings, invoices) {
+    return weddings.filter((wedding) => {
+        return invoices.find(invoice => (invoice.WEDDING_ID === wedding.ID) && (invoice.ACCEPTED !== "TRUE"));
+    });
+}
+
+function filtrarPorData(weddings, dataInicial, dataFinal) {
+    return weddings.filter((wedding) => {
+        return moment(wedding.WEDDING_DATE).isBetween(dataInicial, dataFinal);
+    });
+}
+
+function formataResposta(casamentos) {
+    return casamentos.map((wedding) => {
+        const clientesChurn = {
+            id_usuario: wedding.OWNER_ID,
+            data_casamento: wedding.WEDDING_DATE
+        };
+        return clientesChurn;
+    });
+}
 
 module.exports = {
     async obterCasaisProbabilidadeRejeicao(req, res) {
-        return res.status(200).json({teste: "sucesso"});
+        let weddings = await lejour.getWeddings();
+        let invoices = await lejour.getInvoices();
+
+        let casamentosComData = obtemCasamentosComData(weddings);
+        let casamentosComInvoicesRecusadas = obtemCasamentosComInvoicesRecusadas(casamentosComData, invoices);
+        let casamementosSemInvoiceGerada = obtemCasamentosSemInvoice(casamentosComData, invoices);
+
+        let casamentosQueNaoConverteram = casamentosComInvoicesRecusadas.concat(casamementosSemInvoiceGerada)
+        casamentosQueNaoConverteram = filtrarPorData(casamentosQueNaoConverteram, moment().add(10, 'days'), moment().add(90, 'days'));
+
+        return res.status(200).json(formataResposta(casamentosQueNaoConverteram));
     },
 
     async obterCasamentosPorOrcamento(req, res) {
--- teste_churn.js
+++ teste_churn.js
@@ -16,16 +16,11 @@ let casamentosSemInvoiceGerada = casamentosComData.filter(wedding => {
     return !invoices.find(invoice => invoice.WEDDING_ID == wedding.ID);
 });
 
-console.log("Quantidade de casamentos com data:" + casamentosComData.length);
-console.log("Quantidade de casamentos com invoices recusadas:" + casamentosComInvoicesRecusadas.length);
-console.log("Quantidade de casamentos sem invoices geradas:" + casamentosSemInvoiceGerada.length);
 
 let casamentosQueNaoConverteram = casamentosComInvoicesRecusadas.concat(casamentosSemInvoiceGerada);
-console.log("Quantidade de casamentos que não converteram: " + casamentosQueNaoConverteram.length);
 
 let dataPossivelConversao = moment().add(10, 'days');
 let dataLimiteConversao = moment().add(90, 'days');
-console.log("Data gerada: " + dataLimiteConversao.toString());
 
 casamentosQueNaoConverteram = casamentosQueNaoConverteram.filter((wedding) => {
     return moment(wedding.WEDDING_DATE).isBetween(dataPossivelConversao, dataLimiteConversao);
@@ -36,6 +31,3 @@ casamentosQueNaoConverteram = casamentosQueNaoConverteram.filter((wedding) => {
     };
     return clientesChurn;
 });
-
-console.log(casamentosQueNaoConverteram.length);
-console.log(casamentosQueNaoConverteram);
